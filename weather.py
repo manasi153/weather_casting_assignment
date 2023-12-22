be02bf2bd4aeed0e53b7a1ab716be00613a7a4a9
manasi153 <manasisarangpathak22@gmail.com>
2023-12-22 17:39:53 +0530

    Added changes for the same

--- weather.py
+++ weather.py
@@ -10,9 +10,49 @@ from functions import fetch_data,query_data,store_data
 
 ACTIONS = ["Fetch", "Query"]
 
+import requests
+import pymongo
+import time
+import os
+from dotenv import load_dotenv
+
+api_key = os.environ.get('YOUR_API_KEY')
+db_name = os.environ.get('weather_data')
+col_name = os.environ.get('Weather')
+
+client = pymongo.MongoClient("mongodb://localhost:27017/") 
+db = client.weather_data     
+            
+def fetch_data(city):
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
+    data = response.json()
+    if (response.status_code==200):
+        return data
+    else:
+        return {}
+
+def store_data(data):
+    temp_k = data['weather_data']['temp']
+    temp_celsius = int(temp_k - 273.15)
+    temp_fahrenheit = int((temp_celsius * 9/5) + 32)
+    data["temp_celsius"] = temp_celsius
+    data["temp_fahrenheit"] = temp_fahrenheit
+    data["timestamp"] = time.time()
+    db["Weather"].insert_one(data)
+
+def query_data(city):
+    result = db["Weather"].find_one({"name":city})
+    if result:
+        print(f"Weather data for {city}:")
+        print(result)
+    elif not result:
+        print(f"Invalid {city}")
+    else:
+        print(f"No data found for the {city}")
+ 
 
 class weatherCast(object):
-    def on_post(self, req, resp):
+    def on_post():
         try:
             parser = argparse.ArgumentParser(description="Fetch, Store and display weather data from OpenWeatherMap API")
             parser.add_argument("--city", help="City name for fetch or query")
@@ -20,14 +60,14 @@ class weatherCast(object):
             args = parser.parse_args()
     
 
-            if ACTIONS in "Fetch":
+            if "Fetch" in ACTIONS:
                 if not args.city:
                     print("Please provide a city name for fetching.")
                     return
                 weather_data = fetch_data(args.city)
                 store_data(weather_data)
                 print(f"Weather data for {args.city} fetched and stored in MongoDB.")
-            elif ACTIONS in "Query":
+            elif "Query" in ACTIONS:
                 if not args.city:
                     print("Please provide a city name for querying.")
                     return
@@ -38,4 +78,5 @@ class weatherCast(object):
         except Exception as e:
             traceback.print_exc()
             
-            
+    if __name__ == "__main__":
+        on_post()            
